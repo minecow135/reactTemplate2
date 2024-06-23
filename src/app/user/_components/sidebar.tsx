6f2d1b450041072f281d978a2c7674d5b793f100
mine_cow135 <40334322+minecow135@users.noreply.github.com>
2024-06-23 01:16:42 +0000

    add bold when active. add no overflow

--- src/app/user/_components/sidebar.tsx
+++ src/app/user/_components/sidebar.tsx
@@ -1,37 +1,48 @@
 import Link from "next/link";
 import { getServerAuthSession } from "~/server/auth";
-import { Separator } from "~/components/ui/separator"
 
 async function Nav() {
   const session = await getServerAuthSession();
 
   const links = [
-    { href: '/user', label: 'Profile' },
+    { href: '/user', label: 'Profile', active: true},
     { href: '/user/settings', label: 'Settings' },
   ]
   return (
-    <nav className="flex flex-col align-top justify-between h-full w-full">
+    <div className="flex flex-col align-top justify-between h-full w-full">
       <div>
       {links.map((link) => (
-        <div key={link.label}>
-          <Link
-          href={link.href}
-            className="block p-1 w-full border-b border-foreground"
-          >
-            {link.label}
-          </Link>
-          <Separator className=" w-11/12 " />
-        </div>
+        link.active ?
+          <div key={link.label}>
+            <Link
+              href={link.href}
+              className="block p-1 w-full"
+            >
+            <p className="border-b border-foreground truncate font-extrabold">{link.label}</p>
+            </Link>
+          </div>
+        :
+          <div key={link.label}>
+            <Link
+              href={link.href}
+              className="block p-1 w-full"
+            >
+              <p className="border-b border-foreground truncate">{link.label}</p>
+            </Link>
+          </div>
       ))}
       </div>
-    </nav>
+    </div>
   )
 }
 
 export default async function Sidebar() {
 
   return (
-    <nav className="flex-1 flex w-full items-center justify-between border-b p-2 m-5 text-xl font-semibold bg-muted/40 rounded-lg">
+    <nav className="items-center justify-between border-b p-2 m-5 text-xl font-semibold bg-muted/40 rounded-lg w-60">
+      <div>
+        
+      </div>
       <Nav />
     </nav>
   );
