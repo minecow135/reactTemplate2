f8cbc210b32beb99fd74672bf08a9db8f8b28e86
mine_cow135 <40334322+minecow135@users.noreply.github.com>
2024-07-12 20:54:15 +0000

    started gallery

---
+++ src/app/admin/img/page.tsx
@@ -0,0 +1,14 @@
+import { Separator } from "~/components/ui/separator"
+
+export default async function Home() {
+  return (
+    <main className="flex w-full h-full flex-grow flex-col items-center mt-5">
+      <h1 className="font-extrabold tracking-tight sm:text-[3rem]">Gallery admin</h1>
+      <Separator className="my-4 w-10/12" />
+
+      <div className="flex flex-col w-4/5">
+        
+      </div>
+    </main>
+  );
+};
