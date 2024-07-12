80385368d8444a3118bc16449b6229558a32ed38
mine_cow135 <40334322+minecow135@users.noreply.github.com>
2024-07-12 19:21:38 +0000

    removed user info temorarily because an error

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,10 +1,10 @@
 import { Separator } from "~/components/ui/separator"
 
-import { userInfo } from "~/server/queries";
+//import { userInfo } from "~/server/queries";
 
 export default async function Settings() {
-  let userData = await userInfo();
-
+  //let userData = await userInfo();
+  
   return (
     <main className="flex w-full h-full flex-grow flex-col items-center mt-5">
       <h1 className="font-extrabold tracking-tight sm:text-[3rem]">Profile</h1>
@@ -12,14 +12,14 @@ export default async function Settings() {
 
       <div className="flex flex-col w-4/5">
         <div className="w-full">
-          <img className="rounded-full mb-10 mr-10 float-left w-1/4 border" src={(userData?.image as string)} alt="profile picture" />
+          <img className="rounded-full mb-10 mr-10 float-left w-1/4 border" src="" alt="profile picture" />
           <div>
             <span>Username: </span>
-            <span>{userData?.name}</span> 
+            <span></span>
           </div>
           <div>
             <span>Email: </span>
-            <span>{userData?.email}</span>
+            <span></span>
           </div>
         </div>
       </div>
