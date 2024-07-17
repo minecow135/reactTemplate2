c963679607cb4426bd8caedd951b67979cfd437f
mine_cow135 <40334322+minecow135@users.noreply.github.com>
2024-07-17 02:57:45 +0000

    fixed any types. add full structure in link.

--- src/app/web/img/[[...gallery]]/page.tsx
+++ src/app/web/img/[[...gallery]]/page.tsx
@@ -3,17 +3,31 @@ import Link from "next/link"
 import { Separator } from "~/components/ui/separator"
 import { getAlbum, getImg } from "~/server/db/img/queries/img"
 
-async function Album(props: any) {
-  let slug = props.props.gallery
+async function Album({ params } : { params: { gallery?: string[] } }) {
+  let slug = params?.gallery
   
   let album = []
   
   if (slug) {
-    let slugNum: number = +slug;
+    let lastSlug = slug[slug.length - 1];
+    if (!lastSlug) {
+      console.error("lastSlug is not defined");
+      return (
+        <div>
+          <h1>Error</h1>
+          <p>lastSlug is not defined</p>
+        </div>
+      );
+    };
+    let slugNum: number = +lastSlug;
     album = await getAlbum(slugNum);
   } else {
     album = await getAlbum(NaN);
   }
+
+  console.log(slug ? slug[slug?.length - 1] : "")
+
+  let a = slug ? slug[slug?.length - 1] : ""
 
   return (
     <div className="flex w-full h-full flex-grow flex-col items-center">
@@ -23,7 +37,7 @@ async function Album(props: any) {
             <h2 className="font-extrabold tracking-tight sm:text-[2rem]">Albums</h2>
             <div className="flex flex-1 flex-wrap justify-evenly w-full">
               {album.map((data: any) => (
-                <Link href={"/web/img/" + data.id} key={data.id} className="flex flex-col justify-center min-w-64 m-5 p-5 bg-muted rounded-lg items-center shadow-md shadow-muted/70">
+                <Link href={(a ? a : "img") + "/" + data.id} key={data.id} className="flex flex-col justify-center min-w-64 m-5 p-5 bg-muted rounded-lg items-center shadow-md shadow-muted/70">
                   {
                     data.img
                       ?
@@ -49,13 +63,23 @@ async function Album(props: any) {
   )
 }
 
-async function Img(props: any) {
-  let slug = props.props.gallery
+async function Img({ params } : { params: { gallery?: string[] } }) {
+  let slug = params?.gallery;
 
-  let img = []
+  let img = [];
 
   if (slug) {
-    let slugNum: number = +slug;
+    let lastSlug = slug[slug.length - 1];
+    if (!lastSlug) {
+      console.error("lastSlug is not defined");
+      return (
+        <div>
+          <h1>Error</h1>
+          <p>lastSlug is not defined</p>
+        </div>
+      );
+    };
+    let slugNum: number = +lastSlug;
     img = await getImg(slugNum);
   } else {
     img = await getImg(NaN);
@@ -95,15 +119,15 @@ async function Img(props: any) {
   )
 }
 
-export default async function Home({ params }: { params: { slug: string } }) {
+export default async function Home({ params }: { params: { gallery: string[] } }) {  
   return (
     <main className="flex w-full h-full flex-grow flex-col items-center mt-5">
       <h1 className="font-extrabold tracking-tight sm:text-[3rem]">Gallery</h1>
       <Separator className="my-4 w-10/12" />
 
       <div className="flex flex-col w-4/5">
-        <Album props={params} />
-        <Img props={params} />
+        <Album params={params} />
+        <Img params={params} />
       </div>
     </main>
   );
